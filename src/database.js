1e6558e211515655d87e27b83f167df20f22cd35
Matias Mattig <matu150503@gmail.com>
2024-03-29 17:50:35 -0300

    CommitPE2P8

--- src/database.js
+++ src/database.js
@@ -1,7 +1,9 @@
 //Conexion con MONGODB
 
 const mongoose = require("mongoose");
+const configObject = require("./config/config.js");
+const {mongo_url} = configObject;
 
-mongoose.connect("mongodb+srv://matumattig:mtqGxGWURhbuI334@cluster0.ssuy9kq.mongodb.net/ecommerce?retryWrites=true&w=majority")
+mongoose.connect(mongo_url)
     .then(() => console.log("Conexion con la base de datos exitosa"))
     .catch(() => console.log("Error al conectar con la base de datos"))
