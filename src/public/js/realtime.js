e93a8d112f17edecd22fcc48c1a8f6cea37be1df
Matias Mattig <matu150503@gmail.com>
2024-04-17 04:02:07 -0300

    Tercer preentrega 4c

--- src/public/js/realtime.js
+++ src/public/js/realtime.js
@@ -2,7 +2,7 @@ const socket = io();
 
 socket.on("products", (data) => {
     renderProducts(data);
-})
+});
 
 // Función para renderizar nuestros productos
 const renderProducts = (products) => {
@@ -32,11 +32,11 @@ const renderProducts = (products) => {
             openUpdateForm(productToUpdate);
         });
     });
-}
+};
 
 const removeProduct = (id) => {
     socket.emit("removeProduct", id);
-}
+};
 
 const addProduct = () => {
     const product = {
@@ -51,7 +51,7 @@ const addProduct = () => {
     };
 
     socket.emit("addProduct", product);
-}
+};
 
 function openUpdateForm(product) {
     const updateForm = document.createElement("form");
@@ -83,69 +83,12 @@ function openUpdateForm(product) {
             category: document.getElementById("updateCategory").value,
             status: document.getElementById("updateStatus").value === "true",
         };
-        await updateProduct(product._id, updatedProduct);
+
+        socket.emit("updateProduct", { id: product._id, updatedFields: updatedProduct });
         
         updateForm.reset();
         updateForm.remove();
     });
 
     document.getElementById("productContainer").appendChild(updateForm);
-}
-
-// const socket = io(); 
-
-// socket.on("products", (data) => {
-//     //console.log(data);
-//     renderProducts(data);
-// })
-
-// //Función para renderizar nuestros productos: 
-
-// const renderProducts = (products) => {
-//     const productContainer = document.getElementById("productContainer");
-//     productContainer.innerHTML = "";
-    
-//     products.docs.forEach(item => {
-//         const card = document.createElement("div");
-//         card.classList.add("card");
-
-//         card.innerHTML = ` 
-//                         <p> ${item.title} </p>
-//                         <p> ${item.price} </p>
-//                         <button> Eliminar </button>
-//                         `;
-
-//         productContainer.appendChild(card);
-//         //Agregamos el evento al boton de eliminar: 
-//         card.querySelector("button").addEventListener("click", ()=> {
-//             removeProduct(item._id);
-//         })
-//     })
-// }
-
-
-// const removeProduct = (id) =>  {
-//     socket.emit("removeProduct", id);
-// }
-
-// //Agregamos productos del formulario: 
-
-// document.getElementById("btnEnviar").addEventListener("click", () => {
-//     addProduct();
-// })
-
-
-// const addProduct = () => {
-//     const product = {
-//         title: document.getElementById("title").value,
-//         description: document.getElementById("description").value,
-//         price: document.getElementById("price").value,
-//         img: document.getElementById("img").value,
-//         code: document.getElementById("code").value,
-//         stock: document.getElementById("stock").value,
-//         category: document.getElementById("category").value,
-//         status: document.getElementById("status").value === "true",
-//     };
-
-//     socket.emit("addProduct", product);
-// }
+};
